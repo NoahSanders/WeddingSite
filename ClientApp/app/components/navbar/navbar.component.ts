d7df47f0f81f88cc32ce369f0726504c8e39e496
Noah Sanders <noahcsan@gmail.com>
2017-04-12 20:27:37 -0500

    first commit, home frame and navbar v1 added

---
+++ ClientApp/app/components/navbar/navbar.component.ts
@@ -0,0 +1,9 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.css']
+})
+export class NavBarComponent {
+}
